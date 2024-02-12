50f7f8c727000bff895c5fc224980ff6713a1bef
Anna Chernova <annanvks@gmail.com>
2024-02-12 20:08:16 +0000

    add input validation for Employee name, id, email

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,6 +1,19 @@
 //This code was created to define and export the Employee class
 class Employee {
   constructor(name, id, email) {
+    // Check if 'name' is a string + non-empty
+    if (typeof name !== "string" || name.trim() === "") {
+      throw new Error("Enter a non-empty string as a name");
+    }
+    // Check if 'id' is a positive number
+    if (!Number.isInteger(id) || id <= 0) {
+      throw new Error("Enter a positive integer as id");
+    }
+    // Check if email has a correct format
+    if (!/^\S+@\S+\.\S+$/.test(email)) {
+      throw new Error("Invalid e-mail format");
+    }
+
     this.name = name;
     this.id = id;
     this.email = email;
