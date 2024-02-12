2740e66929d738aab40615bcfd5fc2129c3bc86d
Anna Chernova <annanvks@gmail.com>
2024-02-12 21:03:09 +0000

    add manager prompt

--- starter/index.js
+++ starter/index.js
@@ -10,6 +10,42 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const team = [];
+
+function teamManagerPrompt() {
+  console.log("Provide the team manager’s information");
+  inquirer
+    .prompt([
+      { type: "input", name: "name", message: "Enter Manager's Name:" },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Office number:",
+      },
+    ])
+    .then((answers) => {
+      let manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      team.push(manager);
+    });
+}
+
+function menuPrompt() {
+    
+}
